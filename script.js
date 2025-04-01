60d41cb9a89d5b19a98031cd24ad1feae2e402de
heloisakuntz <167546781+heloisakuntz@users.noreply.github.com>
2025-04-01 08:29:38 -0300

    01/04

--- script.js
+++ script.js
@@ -6,5 +6,5 @@ document.addEventListener('DOMContentLoaded', function(){
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
-    })
-})
+    });
+});
